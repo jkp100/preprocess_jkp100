4337ada746c234fc8136f4f0f0e04694ef47958f
jkp100 <137459213+jkp100@users.noreply.github.com>
2023-09-06 18:58:14 -0400

    python package for https://bit.ly/intro_nlp

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'preprocess_jkp100', # this should be unique)
+	version = '0.0.1',
+	author = 'Johnny Panasy',
+	author_email = johnnypanasy@gmail.com,
+	description = 'This is complete preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown'
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Approved :: MIT License',
+	"Operating System" :: OS Independent"],
+	python_requires = '>=3.5'
+	) 
